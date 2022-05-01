f7ddc7f5ab81cca6a84c9730035cc1cca072305a
Nailik <13292441+Nailik@users.noreply.github.com>
2022-05-01 17:25:19 +0200

    revamped settings structure

--- MultiPlatformLibrary/src/commonMain/kotlin/org/rhasspy/mobile/settings/ConfigurationSetting.kt
+++ MultiPlatformLibrary/src/commonMain/kotlin/org/rhasspy/mobile/settings/ConfigurationSetting.kt
@@ -1,44 +1,32 @@
 package org.rhasspy.mobile.settings
 
 import com.russhwolf.settings.ExperimentalSettingsApi
-import com.russhwolf.settings.get
-import com.russhwolf.settings.serialization.decodeValue
-import com.russhwolf.settings.serialization.encodeValue
-import com.russhwolf.settings.set
 import dev.icerock.moko.mvvm.livedata.MutableLiveData
 import dev.icerock.moko.mvvm.livedata.postValue
+import dev.icerock.moko.mvvm.livedata.readOnly
+import kotlinx.coroutines.launch
 import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.builtins.SetSerializer
-import kotlinx.serialization.builtins.serializer
-import org.rhasspy.mobile.data.DataEnum
 import org.rhasspy.mobile.viewModels.GlobalData
 
-class ConfigurationSetting<T>(private val key: SettingsEnum, private val initial: T) {
+/**
+ * configuration settings do not change there saved value when value is changed,
+ * internally unsaved value will be changed and on save it's saved to the value
+ */
+class ConfigurationSetting<T>(key: SettingsEnum, initial: T) : Setting<T>(key, initial) {
 
     init {
-        GlobalData.allSettings.add(this)
+        GlobalData.allConfigurationSettings.add(this)
     }
 
-    @OptIn(ExperimentalSerializationApi::class, ExperimentalSettingsApi::class)
-    val value: T
-        get() {
-            @Suppress("UNCHECKED_CAST")
-            return when (initial) {
-                is String -> GlobalData.settings[key.name, initial]
-                is Int -> GlobalData.settings[key.name, initial]
-                is Float -> GlobalData.settings[key.name, initial]
-                is Boolean -> GlobalData.settings[key.name, initial]
-                is DataEnum<*> -> initial.findValue(GlobalData.settings[key.name, initial.name])
-                is Set<*> -> GlobalData.settings.decodeValue(SetSerializer(String.serializer()), key.name, initial as Set<String>)
-                else -> throw RuntimeException()
-            } as T
-        }
+    //if there are unsaved changes in this value
+    private val isUnsaved = MutableLiveData(false)
+    val hasUnsavedChange = isUnsaved.readOnly()
 
-    var data: T = value
-        private set
-
-    val unsaved = object : MutableLiveData<T>(value) {
-        @Suppress("UNCHECKED_CAST")
+    /**
+     * holds the unsaved value
+     * updated unsaved changes accordingly if the new value is different or the same as the current saved value
+     */
+    private val unsaved = object : MutableLiveData<T>(readValue()) {
         override var value: T
             get() = super.value
             set(newValue) {
@@ -46,43 +34,49 @@ class ConfigurationSetting<T>(private val key: SettingsEnum, private val initial
                     super.value = newValue
                     if (data != newValue) {
                         //new value
-                        unsavedChange.value = true
+                        isUnsaved.value = true
                         GlobalData.unsavedChanges.value = true
                     } else {
                         //set value back to saved
-                        unsavedChange.value = false
+                        isUnsaved.value = false
                         GlobalData.updateUnsavedChanges()
                     }
                 }
             }
     }
 
-    val unsavedChange = MutableLiveData(false)
+    /**
+     * data used to get current saved value or to set value for unsaved changes
+     */
+    override var data: T
+        get() = this.value.value
+        set(newValue) {
+            viewScope.launch {
+                unsaved.value = newValue
+            }
+        }
 
+    /**
+     * data that's live updated to get current value readonly
+     */
+    var liveUnsavedData = unsaved.readOnly()
+
+    /**
+     * saves the current unsaved value and resets unsaved changes and updates the value
+     */
     @OptIn(ExperimentalSerializationApi::class, ExperimentalSettingsApi::class)
     fun save() {
-        @Suppress("UNCHECKED_CAST")
-        when (initial) {
-            is String -> GlobalData.settings[key.name] = unsaved.value as String
-            is Int -> GlobalData.settings[key.name] = unsaved.value as Int
-            is Float -> GlobalData.settings[key.name] = unsaved.value as Float
-            is Boolean -> GlobalData.settings[key.name] = unsaved.value as Boolean
-            is DataEnum<*> -> GlobalData.settings[key.name] = (unsaved.value as DataEnum<*>).name
-            is Set<*> -> GlobalData.settings.encodeValue(SetSerializer(String.serializer()), key.name, initial as Set<String>)
-            else -> throw RuntimeException()
-        }
-        unsavedChange.postValue(false)
-        data = unsaved.value
+        //update unsaved changes
+        isUnsaved.postValue(false)
+        //update saved value
+        value.postValue(unsaved.value)
     }
 
+    /**
+     * reset unsaved changes to the current value
+     */
     fun reset() {
-        unsaved.value = value
+        unsaved.value = value.value
     }
-
-    var unsavedData: T
-        get() = this.unsaved.value
-        set(newValue) {
-            this.unsaved.value = newValue
-        }
 
 }
---
+++ MultiPlatformLibrary/src/commonMain/kotlin/org/rhasspy/mobile/settings/Setting.kt
@@ -0,0 +1,81 @@
+package org.rhasspy.mobile.settings
+
+import com.russhwolf.settings.ExperimentalSettingsApi
+import com.russhwolf.settings.get
+import com.russhwolf.settings.serialization.decodeValue
+import com.russhwolf.settings.serialization.encodeValue
+import com.russhwolf.settings.set
+import dev.icerock.moko.mvvm.livedata.MutableLiveData
+import dev.icerock.moko.mvvm.livedata.readOnly
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.builtins.SetSerializer
+import kotlinx.serialization.builtins.serializer
+import org.rhasspy.mobile.data.DataEnum
+import org.rhasspy.mobile.viewModels.GlobalData
+
+abstract class Setting<T>(private val key: SettingsEnum, private val initial: T) {
+
+    companion object {
+        internal val viewScope = CoroutineScope(Dispatchers.Main)
+    }
+
+    /**
+     * returns value as a mutable live data to observe it
+     */
+    @OptIn(ExperimentalSerializationApi::class, ExperimentalSettingsApi::class)
+    internal val value = object : MutableLiveData<T>(readValue()) {
+        override var value: T
+            get():T = super.value
+            set(newValue) {
+                if (value != newValue) {
+                    @Suppress("UNCHECKED_CAST")
+                    when (initial) {
+                        is String -> GlobalData.settings[key.name] = newValue as String
+                        is Int -> GlobalData.settings[key.name] = newValue as Int
+                        is Float -> GlobalData.settings[key.name] = newValue as Float
+                        is Boolean -> GlobalData.settings[key.name] = newValue as Boolean
+                        is DataEnum<*> -> GlobalData.settings[key.name] = (newValue as DataEnum<*>).name
+                        is Set<*> -> GlobalData.settings.encodeValue(SetSerializer(String.serializer()), key.name, newValue as Set<String>)
+                        else -> throw RuntimeException()
+                    }
+                    super.value = newValue
+                }
+            }
+    }
+
+    /**
+     * reads current saved value
+     */
+    @OptIn(ExperimentalSerializationApi::class, ExperimentalSettingsApi::class)
+    internal fun readValue(): T {
+        @Suppress("UNCHECKED_CAST")
+        return when (initial) {
+            is String -> GlobalData.settings[key.name, initial]
+            is Int -> GlobalData.settings[key.name, initial]
+            is Float -> GlobalData.settings[key.name, initial]
+            is Boolean -> GlobalData.settings[key.name, initial]
+            is DataEnum<*> -> initial.findValue(GlobalData.settings[key.name, initial.name])
+            is Set<*> -> GlobalData.settings.decodeValue(SetSerializer(String.serializer()), key.name, initial as Set<String>)
+            else -> throw RuntimeException()
+        } as T
+    }
+
+    /**
+     * represents current saved data and when changed, updates data asynchronously
+     */
+    open var data: T
+        get() = this.value.value
+        set(newValue) {
+            viewScope.launch {
+                value.value = newValue
+            }
+        }
+
+    /**
+     * data that's live updated to get current value readonly
+     */
+    var liveData = value.readOnly()
+}
