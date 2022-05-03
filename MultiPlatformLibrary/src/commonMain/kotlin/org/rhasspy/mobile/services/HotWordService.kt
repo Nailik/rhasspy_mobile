dcfbdfedada0bca4452a0fe447dafa3263899839
Nailik <13292441+Nailik@users.noreply.github.com>
2022-05-03 21:34:15 +0200

    HotWordService.kt redesigned

--- MultiPlatformLibrary/src/commonMain/kotlin/org/rhasspy/mobile/services/HotWordService.kt
+++ MultiPlatformLibrary/src/commonMain/kotlin/org/rhasspy/mobile/services/HotWordService.kt
@@ -3,10 +3,11 @@ package org.rhasspy.mobile.services
 import co.touchlab.kermit.Logger
 import com.badoo.reaktive.observable.doOnAfterNext
 import org.rhasspy.mobile.data.WakeWordOption
+import org.rhasspy.mobile.logic.State
+import org.rhasspy.mobile.logic.StateMachine
+import org.rhasspy.mobile.nativeutils.NativeLocalWakeWordService
 import org.rhasspy.mobile.services.MqttService.hotWordError
-import org.rhasspy.mobile.services.logic.State
-import org.rhasspy.mobile.services.logic.StateMachine
-import org.rhasspy.mobile.services.native.NativeLocalWakeWordService
+import org.rhasspy.mobile.settings.AppSettings
 import org.rhasspy.mobile.settings.ConfigurationSettings
 import kotlin.native.concurrent.ThreadLocal
 
@@ -15,6 +16,7 @@ import kotlin.native.concurrent.ThreadLocal
  *
  * according to this it starts and stops the native hot word service or eventually starts recording to send the speech data to mqtt
  */
+
 @ThreadLocal
 object HotWordService {
     private val logger = Logger.withTag("HotWordService")
@@ -22,21 +24,40 @@ object HotWordService {
 
     /**
      * starts the service
-     * observes state machine to start and stop recording for wake word
+     * observes state machine and hotWordEnabled setting
+     * to start and stop recording for wake word
      */
     init {
+        AppSettings.isHotWordEnabled.observableData.doOnAfterNext {
+            logger.v { "isHotWordEnabled changed to $it" }
+            evaluateHotWorAction(StateMachine.state, it)
+        }
+
         //change things according to state of the service
         StateMachine.currentState.doOnAfterNext {
-            when (it) {
-                State.AwaitingHotWord -> {
+            logger.v { "currentState changed to $it" }
+            evaluateHotWorAction(it, AppSettings.isHotWordEnabled.data)
+        }
+    }
+
+    private fun evaluateHotWorAction(state: State, enabled: Boolean) {
+        logger.v { "evaluateHotWorAction state $state enabled $enabled running $isRunning" }
+        when (state) {
+            State.AwaitingHotWord -> {
+                if (enabled) {
                     if (!isRunning) {
-                        startHotWord()
+                        //enabled and not running
+                        start()
                     }
+                } else if (isRunning) {
+                    //not enabled but running
+                    stop()
                 }
-                else -> {
-                    if (isRunning) {
-                        stopHotWord()
-                    }
+            }
+            else -> {
+                if (isRunning) {
+                    //in another state but running
+                    stop()
                 }
             }
         }
@@ -45,7 +66,7 @@ object HotWordService {
     /**
      * start hotWord services
      */
-    private fun startHotWord() {
+    private fun start() {
         isRunning = true
         logger.d { "startHotWord" }
 
@@ -70,7 +91,7 @@ object HotWordService {
      * stop hotWord services
      * doesn't check for config setting because they might have changed, stop all the hotWord Services
      */
-    private fun stopHotWord() {
+    private fun stop() {
         isRunning = false
 
         logger.d { "stopHotWord" }
