5b975f9704a43a9cce32a73591a27da9810db23e
Nailik <13292441+Nailik@users.noreply.github.com>
2022-05-14 18:19:25 +0200

    remoed unused files

--- MultiPlatformLibrary/src/commonMain/kotlin/org/rhasspy/mobile/nativeutils/LibrariesUtils.kt
+++
@@ -1,5 +0,0 @@
-package org.rhasspy.mobile.nativeutils
-
-import com.mikepenz.aboutlibraries.entity.Library
-
-expect suspend fun loadLibraries(): List<Library>
--- MultiPlatformLibrary/src/iosMain/kotlin/org/rhasspy/mobile/nativeutils/LibrariesUtils.kt
+++
@@ -1,7 +0,0 @@
-package org.rhasspy.mobile.nativeutils
-
-import com.mikepenz.aboutlibraries.entity.Library
-
-actual suspend fun loadLibraries(): List<Library> {
-    TODO("Not yet implemented")
-}
