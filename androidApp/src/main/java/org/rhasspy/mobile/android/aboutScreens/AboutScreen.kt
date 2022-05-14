3f397d0c56a2afdf48fed50aadeccf699a791416
Nailik <13292441+Nailik@users.noreply.github.com>
2022-05-14 18:19:18 +0200

    new about screen

--- androidApp/src/main/java/org/rhasspy/mobile/android/aboutScreens/AboutScreen.kt
+++ androidApp/src/main/java/org/rhasspy/mobile/android/aboutScreens/AboutScreen.kt
@@ -2,6 +2,7 @@ package org.rhasspy.mobile.android.aboutScreens
 
 import android.content.Intent
 import android.net.Uri
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
@@ -12,10 +13,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import androidx.core.content.pm.PackageInfoCompat
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import org.rhasspy.mobile.BuildKonfig
 import org.rhasspy.mobile.MR
 import org.rhasspy.mobile.android.theme.getIsDarkTheme
 import org.rhasspy.mobile.android.utils.Icon
@@ -23,39 +25,37 @@ import org.rhasspy.mobile.android.utils.Text
 import org.rhasspy.mobile.android.utils.translate
 
 //git commits for changelog https://lowcarbrob.medium.com/android-pro-tip-generating-your-apps-changelog-from-git-inside-build-gradle-19a07533eec4
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun AboutScreen() {
-
+    rememberSystemUiController().setStatusBarColor(MaterialTheme.colorScheme.surfaceVariant, darkIcons = !getIsDarkTheme())
     Surface {
-
-        Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxWidth()) {
-
-            Header()
-
-            LibrariesContainer()
-
-        }
+        val configuration = LocalConfiguration.current
+        LibrariesContainer(header = {
+            if (configuration.screenHeightDp.dp > 600.dp) {
+                stickyHeader {
+                    Header()
+                }
+            } else {
+                item {
+                    Header()
+                }
+            }
+        })
     }
-
 }
+
 
 @Composable
 fun Header() {
-
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .fillMaxWidth()
             .background(MaterialTheme.colorScheme.surfaceVariant)
-            .padding(vertical = 8.dp)
+            .height(300.dp)
+            .padding(vertical = 16.dp)
     ) {
-        val context = LocalContext.current
-
-        val manager = context.packageManager
-        val info = manager.getPackageInfo(context.packageName, 0)
-        val versionName = info.versionName
-        val versionCode = PackageInfoCompat.getLongVersionCode(info).toInt()
-        rememberSystemUiController().setStatusBarColor(MaterialTheme.colorScheme.surfaceVariant, darkIcons = !getIsDarkTheme())
         Box(
             modifier = Modifier
                 .background(MaterialTheme.colorScheme.surface, shape = CircleShape)
@@ -67,13 +67,12 @@ fun Header() {
         Text(MR.strings.appName, style = MaterialTheme.typography.headlineLarge, modifier = Modifier.padding(8.dp))
 
         Text(
-            "${translate(MR.strings.version)} $versionName - $versionCode",
+            "${translate(MR.strings.version)} ${BuildKonfig.versionName} - ${BuildKonfig.versionCode}",
             style = MaterialTheme.typography.bodyMedium,
             modifier = Modifier.padding(8.dp)
         )
 
         AppInformationChips()
-
     }
 }
 
@@ -83,7 +82,6 @@ fun AppInformationChips() {
 
     Row(
         modifier = Modifier
-            .padding(vertical = 8.dp)
             .fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly
     ) {
         DataPrivacyDialogueButton()
