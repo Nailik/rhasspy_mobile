b91fd188d07886992490047659db1acf302a2f8d
Nailik <13292441+Nailik@users.noreply.github.com>
2022-05-27 14:28:55 +0200

    removed badoo from gradle

--- MultiPlatformLibrary/build.gradle.kts
+++ MultiPlatformLibrary/build.gradle.kts
@@ -61,8 +61,6 @@ kotlin {
                 implementation(Ktor2.Server.dataConversion)
                 implementation(Ktor2.Server.cio)
                 implementation(Benasher.uuid)
-                implementation(Badoo.reaktive)
-                implementation(Badoo.coroutinesInterop)
             }
         }
         val commonTest by getting {
