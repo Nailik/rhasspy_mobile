6eee4286b87886e43b87c58a1735213ee73ed917
Nailik <13292441+Nailik@users.noreply.github.com>
2022-05-27 14:15:18 +0200

    removed unused badoo reference

--- buildSrc/src/main/kotlin/Badoo.kt
+++
@@ -1,8 +0,0 @@
-import de.fayard.refreshVersions.core.DependencyGroup
-
-object Badoo : DependencyGroup(group = "com.badoo.reaktive") {
-
-    val reaktive = module("reaktive")
-    val coroutinesInterop = module("coroutines-interop")
-
-}
