9873d0773b6fd0bd55684bf2fdb5727575ed5c43
Nailik <13292441+Nailik@users.noreply.github.com>
2022-06-01 23:57:35 +0200

    updated refresh versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,8 +8,7 @@ pluginManagement {
 
 plugins {
     // See https://jmfayard.github.io/refreshVersions
-    id("de.fayard.refreshVersions") version "0.40.1"
-////                            # available:"0.40.2"
+    id("de.fayard.refreshVersions") version "0.40.2"
 }
 
 rootProject.name = "Rhasspy_Mobile"
