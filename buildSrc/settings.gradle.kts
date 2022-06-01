ab87419a0fb789a1291fe82c39be514c0a1f71c8
Nailik <13292441+Nailik@users.noreply.github.com>
2022-06-01 23:58:56 +0200

    updated refresh versions

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -1,7 +1,6 @@
 pluginManagement {
     plugins {
-        id("de.fayard.refreshVersions") version "0.40.1"
-////                                # available:"0.40.2"
+        id("de.fayard.refreshVersions") version "0.40.2"
     }
 }
 
