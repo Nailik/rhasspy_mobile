a5f8177b3751f0afc15d5d6b668fe12173a0d6a1
Nailik <13292441+Nailik@users.noreply.github.com>
2022-06-05 01:43:35 +0200

    fixed no actual for expect warning

--- MultiPlatformLibrary/src/commonMain/kotlin/org/rhasspy/mobile/nativeutils/NativeHttpUtils.kt
+++ MultiPlatformLibrary/src/commonMain/kotlin/org/rhasspy/mobile/nativeutils/NativeHttpUtils.kt
@@ -3,8 +3,11 @@ package org.rhasspy.mobile.nativeutils
 import io.ktor.client.engine.cio.*
 import io.ktor.server.application.*
 
+@Suppress("NO_ACTUAL_FOR_EXPECT")
 expect fun Application.installCompression()
 
+@Suppress("NO_ACTUAL_FOR_EXPECT")
 expect fun Application.installCallLogging()
 
+@Suppress("NO_ACTUAL_FOR_EXPECT")
 expect fun CIOEngineConfig.configureEngine()
