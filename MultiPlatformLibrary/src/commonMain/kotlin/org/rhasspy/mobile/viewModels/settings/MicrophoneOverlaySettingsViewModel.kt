5fd6a4a571f71355f436e87f78d1068d166cf27c
Nailik <13292441+Nailik@users.noreply.github.com>
2022-11-05 15:52:20 +0100

    fixed missing permission request owner

--- MultiPlatformLibrary/src/commonMain/kotlin/org/rhasspy/mobile/viewModels/settings/MicrophoneOverlaySettingsViewModel.kt
+++ MultiPlatformLibrary/src/commonMain/kotlin/org/rhasspy/mobile/viewModels/settings/MicrophoneOverlaySettingsViewModel.kt
@@ -1,42 +1,23 @@
 package org.rhasspy.mobile.viewModels.settings
 
 import dev.icerock.moko.mvvm.viewmodel.ViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import org.rhasspy.mobile.readOnly
 import org.rhasspy.mobile.settings.AppSettings
 
 class MicrophoneOverlaySettingsViewModel : ViewModel() {
 
-    //unsaved data
-    private val _isMicrophoneOverlayEnabled = MutableStateFlow(AppSettings.isMicrophoneOverlayEnabled.value)
-    private val _isMicrophoneOverlayWhileAppEnabled = MutableStateFlow(AppSettings.isMicrophoneOverlayWhileAppEnabled.value)
-
     //unsaved ui data
-    val isMicrophoneOverlayEnabled = _isMicrophoneOverlayEnabled.readOnly
-    val isMicrophoneOverlayWhileAppEnabled = _isMicrophoneOverlayWhileAppEnabled.readOnly
+    val isMicrophoneOverlayEnabled = AppSettings.isMicrophoneOverlayEnabled.data
+    val isMicrophoneOverlayWhileAppEnabledVisible = isMicrophoneOverlayEnabled
+    val isMicrophoneOverlayWhileAppEnabled = AppSettings.isMicrophoneOverlayWhileAppEnabled.data
 
-    //set new intent recognition option
+    //microphone overlay on/off
     fun toggleMicrophoneOverlayEnabled(enabled: Boolean) {
-        _isMicrophoneOverlayEnabled.value = enabled
+        AppSettings.isMicrophoneOverlayEnabled.value = enabled
     }
 
+    //microphone overlay in background on/off
     fun toggleMicrophoneOverlayWhileAppEnabled(enabled: Boolean) {
-        _isMicrophoneOverlayWhileAppEnabled.value = enabled
-    }
-
-    /**
-     * save data configuration
-     */
-    fun save() {
-        AppSettings.isMicrophoneOverlayEnabled.value = _isMicrophoneOverlayEnabled.value
-        AppSettings.isMicrophoneOverlayWhileAppEnabled.value = _isMicrophoneOverlayWhileAppEnabled.value
-    }
-
-    /**
-     * test unsaved data configuration
-     */
-    fun test() {
-
+        AppSettings.isMicrophoneOverlayWhileAppEnabled.value = enabled
     }
 
 }
