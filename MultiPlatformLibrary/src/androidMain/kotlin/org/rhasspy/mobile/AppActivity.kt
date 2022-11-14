8f57cad4d39f53770138bc9e6eb3a7a8f0c19177
Nailik <13292441+Nailik@users.noreply.github.com>
2022-11-14 23:45:48 +0100

    open document opens download folder

--- MultiPlatformLibrary/src/androidMain/kotlin/org/rhasspy/mobile/AppActivity.kt
+++ MultiPlatformLibrary/src/androidMain/kotlin/org/rhasspy/mobile/AppActivity.kt
@@ -1,10 +1,15 @@
 package org.rhasspy.mobile
 
 import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.provider.DocumentsContract.EXTRA_INITIAL_URI
+import android.provider.MediaStore
 import androidx.activity.ComponentActivity
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+
 
 abstract class AppActivity : ComponentActivity() {
 
@@ -23,6 +28,13 @@ abstract class AppActivity : ComponentActivity() {
 
     fun openDocument(types: Array<String>, onResult: (activityResult: ActivityResult) -> Unit) {
         resultCallback = onResult
-        someActivityResultLauncher.launch(ActivityResultContracts.OpenDocument().createIntent(this, types))
+        val intent = ActivityResultContracts.OpenDocument().createIntent(this, types)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            intent.putExtra(EXTRA_INITIAL_URI, MediaStore.Downloads.getContentUri(MediaStore.VOLUME_EXTERNAL))
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val uriDownload: Uri = Uri.parse("content://com.android.externalstorage.documents/document/primary:Download")
+            intent.putExtra(EXTRA_INITIAL_URI, uriDownload)
+        }
+        someActivityResultLauncher.launch(intent)
     }
 }
