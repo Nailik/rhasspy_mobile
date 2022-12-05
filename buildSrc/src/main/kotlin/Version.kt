777d73b6ffffdb753384851f3a15a67f6a320b5d
Nailik <13292441+Nailik@users.noreply.github.com>
2022-12-06 00:11:34 +0100

    initial play store test release

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -3,7 +3,7 @@ object Version {
     const val major = 0
     const val minor = 3
     const val patch = 2
-    const val code = 19
+    const val code = 20
 
     override fun toString(): String {
         return "$major.$minor.$patch-$code"
