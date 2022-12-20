0ca9bad4e0f770280da72a0b2f2062e71ebebbc8
Nailik <13292441+Nailik@users.noreply.github.com>
2022-12-20 17:30:02 +0100

    added information for device settings

--- androidApp/src/main/java/org/rhasspy/mobile/android/settings/content/DeviceSettings.kt
+++ androidApp/src/main/java/org/rhasspy/mobile/android/settings/content/DeviceSettings.kt
@@ -1,6 +1,5 @@
 package org.rhasspy.mobile.android.settings.content
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
@@ -8,6 +7,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import org.koin.androidx.compose.getViewModel
 import org.rhasspy.mobile.MR
 import org.rhasspy.mobile.android.TestTag
+import org.rhasspy.mobile.android.content.list.InformationListElement
 import org.rhasspy.mobile.android.content.list.SliderListItem
 import org.rhasspy.mobile.android.content.list.SwitchListItem
 import org.rhasspy.mobile.android.settings.SettingsScreenItemContent
@@ -15,7 +15,6 @@ import org.rhasspy.mobile.android.settings.SettingsScreens
 import org.rhasspy.mobile.android.testTag
 import org.rhasspy.mobile.viewModels.settings.DeviceSettingsSettingsViewModel
 
-//TODO information that these settings can be changed by calling api (http server enabled) or pushing mqtt topics (mqtt enabled)
 /**
  * Device Settings
  * Volume
@@ -32,41 +31,39 @@ fun DeviceSettingsContent(viewModel: DeviceSettingsSettingsViewModel = getViewMo
         title = MR.strings.device
     ) {
 
-        Column {
+        InformationListElement(text = MR.strings.deviceSettingsLongInformation)
 
-            //volume slider
-            SliderListItem(
-                modifier = Modifier.testTag(TestTag.Volume),
-                text = MR.strings.volume,
-                value = viewModel.volume.collectAsState().value,
-                onValueChange = viewModel::updateVolume
-            )
+        //volume slider
+        SliderListItem(
+            modifier = Modifier.testTag(TestTag.Volume),
+            text = MR.strings.volume,
+            value = viewModel.volume.collectAsState().value,
+            onValueChange = viewModel::updateVolume
+        )
 
-            //hot word enabled
-            SwitchListItem(
-                modifier = Modifier.testTag(TestTag.HotWord),
-                text = MR.strings.hotWord,
-                isChecked = viewModel.isHotWordEnabled.collectAsState().value,
-                onCheckedChange = viewModel::toggleHotWordEnabled
-            )
+        //hot word enabled
+        SwitchListItem(
+            modifier = Modifier.testTag(TestTag.HotWord),
+            text = MR.strings.hotWord,
+            isChecked = viewModel.isHotWordEnabled.collectAsState().value,
+            onCheckedChange = viewModel::toggleHotWordEnabled
+        )
 
-            //audio output enabled
-            SwitchListItem(
-                modifier = Modifier.testTag(TestTag.AudioOutput),
-                text = MR.strings.audioOutput,
-                isChecked = viewModel.isAudioOutputEnabled.collectAsState().value,
-                onCheckedChange = viewModel::toggleAudioOutputEnabled
-            )
+        //audio output enabled
+        SwitchListItem(
+            modifier = Modifier.testTag(TestTag.AudioOutput),
+            text = MR.strings.audioOutput,
+            isChecked = viewModel.isAudioOutputEnabled.collectAsState().value,
+            onCheckedChange = viewModel::toggleAudioOutputEnabled
+        )
 
-            //intent handling enabled
-            SwitchListItem(
-                modifier = Modifier.testTag(TestTag.IntentHandling),
-                text = MR.strings.intentHandling,
-                isChecked = viewModel.isIntentHandlingEnabled.collectAsState().value,
-                onCheckedChange = viewModel::toggleIntentHandlingEnabled
-            )
-
-        }
+        //intent handling enabled
+        SwitchListItem(
+            modifier = Modifier.testTag(TestTag.IntentHandling),
+            text = MR.strings.intentHandling,
+            isChecked = viewModel.isIntentHandlingEnabled.collectAsState().value,
+            onCheckedChange = viewModel::toggleIntentHandlingEnabled
+        )
 
     }
 
