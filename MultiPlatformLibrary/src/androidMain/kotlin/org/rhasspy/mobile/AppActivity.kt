6d4e786077058be535ca9ada33fefd55b8d885f9
Nailik <13292441+Nailik@users.noreply.github.com>
2023-01-10 11:42:44 +0100

    fix open download folder when selecting file

--- MultiPlatformLibrary/src/androidMain/kotlin/org/rhasspy/mobile/AppActivity.kt
+++ MultiPlatformLibrary/src/androidMain/kotlin/org/rhasspy/mobile/AppActivity.kt
@@ -4,12 +4,10 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Build
 import android.provider.DocumentsContract.EXTRA_INITIAL_URI
-import android.provider.MediaStore
 import androidx.activity.ComponentActivity
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-
 
 abstract class AppActivity : ComponentActivity() {
 
@@ -36,14 +34,8 @@ abstract class AppActivity : ComponentActivity() {
     fun openDocument(types: Array<String>, onResult: (activityResult: ActivityResult) -> Unit) {
         resultCallback = onResult
         val intent = ActivityResultContracts.OpenDocument().createIntent(this, types)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            intent.putExtra(
-                EXTRA_INITIAL_URI,
-                MediaStore.Downloads.getContentUri(MediaStore.VOLUME_EXTERNAL)
-            )
-        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val uriDownload: Uri =
-                Uri.parse("content://com.android.externalstorage.documents/document/primary:Download")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val uriDownload: Uri = Uri.parse("content://com.android.externalstorage.documents/document/primary:Download")
             intent.putExtra(EXTRA_INITIAL_URI, uriDownload)
         }
         someActivityResultLauncher.launch(intent)
