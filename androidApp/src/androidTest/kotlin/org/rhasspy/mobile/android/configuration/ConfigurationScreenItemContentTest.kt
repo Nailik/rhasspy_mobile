75bd4e16fc91d6754d639779719e4f20e63f3710
Nailik <13292441+Nailik@users.noreply.github.com>
2023-01-13 12:22:58 +0100

    fix too often false failing test

--- androidApp/src/androidTest/kotlin/org/rhasspy/mobile/android/configuration/ConfigurationScreenItemContentTest.kt
+++ androidApp/src/androidTest/kotlin/org/rhasspy/mobile/android/configuration/ConfigurationScreenItemContentTest.kt
@@ -160,7 +160,8 @@ class ConfigurationScreenItemContentTest {
 
         //app bar back click shows dialog
         composeTestRule.awaitIdle()
-        composeTestRule.onNodeWithTag(TestTag.AppBarBackButton).performClick()
+        device.pressBack()
+        //composeTestRule.onNodeWithTag(TestTag.AppBarBackButton).performClick() TOO often not found while running test
         composeTestRule.onNodeWithTag(TestTag.DialogUnsavedChanges).assertExists()
         //outside click closes dialog
         device.click(300, 300)
