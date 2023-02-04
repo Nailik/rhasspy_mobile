8dd7a7ff4526067a11a408c285bedbfd1aa4ecf3
Nailik <13292441+Nailik@users.noreply.github.com>
2023-02-05 00:24:32 +0100

    added splash view

--- shared/src/iosMain/kotlin/org/rhasspy/mobile/IosApplication.kt
+++ shared/src/iosMain/kotlin/org/rhasspy/mobile/IosApplication.kt
@@ -5,12 +5,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.window.Application
 import dev.icerock.moko.resources.desc.Resource
 import dev.icerock.moko.resources.desc.StringDesc
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.rhasspy.mobile.ui.LocalImage
 import platform.UIKit.UIViewController
 
@@ -18,28 +22,34 @@ import platform.UIKit.UIViewController
 class IosApplication : Application() {
 
     init {
-        onCreated()
+        CoroutineScope(Dispatchers.Default).launch {
+            onCreated()
+        }
     }
 
     @Suppress("FunctionName")
     fun MainViewController(): UIViewController =
         Application("Rhasspy Mobile") { //TODO splash view
             MaterialTheme {
-                Box(modifier = Modifier.fillMaxSize()) {
-                    FilledTonalButton(modifier = Modifier.align(Alignment.Center), onClick = {}) {
-                        Text(text = "Hello Compose Ui! - Material3")
-                        Text(text = StringDesc.Resource(MR.strings.backgroundServiceInformation).localized())
+                if(isHasStarted.collectAsState().value) {
+                    Box(modifier = Modifier.fillMaxSize()) {
+                        FilledTonalButton(modifier = Modifier.align(Alignment.Center), onClick = {}) {
+                            Text(text = "Hello Compose Ui! - Material3")
+                            Text(text = StringDesc.Resource(MR.strings.backgroundServiceInformation).localized())
+                        }
+
                     }
-
+                } else {
+                    Box(modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center) {
+                        LocalImage(
+                            imageResource = MR.images.ic_launcher,
+                            isGreyscale = true,
+                            contentDescription = "text",
+                            contentScale = ContentScale.FillWidth
+                        )
+                    }
                 }
-
-                LocalImage(
-                    imageResource = MR.images.ic_launcher,
-                    isGreyscale = true,
-                    modifier = Modifier,
-                    contentDescription = "text",
-                    contentScale = ContentScale.FillWidth
-                )
             }
         }
 
