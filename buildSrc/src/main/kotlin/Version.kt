569e7af464a3d18ce605648cbf97fc97d3a726c6
Nailik <13292441+Nailik@users.noreply.github.com>
2023-02-05 18:18:09 +0100

    cleanup code and update version to 0.4.8

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -2,7 +2,7 @@ object Version {
 
     const val major = 0
     const val minor = 4
-    const val patch = 7
+    const val patch = 8
     const val code = 62
 
     override fun toString(): String {
