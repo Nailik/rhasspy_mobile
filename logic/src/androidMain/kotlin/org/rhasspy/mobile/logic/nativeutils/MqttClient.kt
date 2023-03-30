4a354dc40fd9643e9f6efdd3df71a2fbbc362efa
Nailik <13292441+Nailik@users.noreply.github.com>
2023-03-30 20:55:30 +0200

    logging

--- logic/src/androidMain/kotlin/org/rhasspy/mobile/logic/nativeutils/MqttClient.kt
+++ logic/src/androidMain/kotlin/org/rhasspy/mobile/logic/nativeutils/MqttClient.kt
@@ -74,7 +74,7 @@ actual class MqttClient actual constructor(
      * to publish and audio frame
      */
     actual suspend fun publish(topic: String, msg: MqttMessage, timeout: Long): MqttError? = try {
-        logger.v { "publish $topic $msg" }
+        logger.v { "publish message to $topic" }
 
         client.publish(topic, org.eclipse.paho.client.mqttv3.MqttMessage(msg.payload).apply {
             id = msg.msgId
