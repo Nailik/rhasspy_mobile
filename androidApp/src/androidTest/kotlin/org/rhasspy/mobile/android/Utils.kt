9952ee4a7e99445e52962cf56e11726fd074ed84
Nailik <13292441+Nailik@users.noreply.github.com>
2023-03-31 08:07:36 +0200

    removed shadowed function

--- androidApp/src/androidTest/kotlin/org/rhasspy/mobile/android/Utils.kt
+++ androidApp/src/androidTest/kotlin/org/rhasspy/mobile/android/Utils.kt
@@ -165,21 +165,3 @@ fun ComposeContentTestRule.waitUntilExists(
 ) {
     return this.waitUntilNodeCount(matcher, 1, timeoutMillis)
 }
-
-@OptIn(ExperimentalTestApi::class)
-fun ComposeContentTestRule.waitUntilDoesNotExist(
-    matcher: SemanticsMatcher,
-    timeoutMillis: Long = 1_000L
-) {
-    return this.waitUntilNodeCount(matcher, 0, timeoutMillis)
-}
-
-fun ComposeContentTestRule.waitUntilNodeCount(
-    matcher: SemanticsMatcher,
-    count: Int,
-    timeoutMillis: Long = 1_000L
-) {
-    this.waitUntil(timeoutMillis) {
-        this.onAllNodes(matcher).fetchSemanticsNodes().size == count
-    }
-}
