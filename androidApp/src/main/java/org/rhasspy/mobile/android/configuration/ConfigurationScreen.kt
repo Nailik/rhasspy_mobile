41d01686f0a5c16cd7dbd7a09effa00109a11ba2
Nailik <13292441+Nailik@users.noreply.github.com>
2023-04-20 19:14:19 +0200

    removed compose koin

--- androidApp/src/main/java/org/rhasspy/mobile/android/configuration/ConfigurationScreen.kt
+++ androidApp/src/main/java/org/rhasspy/mobile/android/configuration/ConfigurationScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.material.icons.filled.Error
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
