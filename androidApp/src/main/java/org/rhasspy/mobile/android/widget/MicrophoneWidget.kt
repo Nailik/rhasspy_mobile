c31577f66a32344cd7de2675330c20a80ed2df12
Nailik <13292441+Nailik@users.noreply.github.com>
2023-05-10 22:22:49 +0200

    dependency update

--- androidApp/src/main/java/org/rhasspy/mobile/android/widget/MicrophoneWidget.kt
+++ androidApp/src/main/java/org/rhasspy/mobile/android/widget/MicrophoneWidget.kt
@@ -1,6 +1,6 @@
 package org.rhasspy.mobile.android.widget
 
-import androidx.compose.runtime.Composable
+import android.content.Context
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.unit.dp
@@ -10,6 +10,7 @@ import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.SizeMode
 import androidx.glance.appwidget.action.actionRunCallback
 import androidx.glance.appwidget.appWidgetBackground
+import androidx.glance.appwidget.provideContent
 import androidx.glance.layout.*
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.get
@@ -21,40 +22,23 @@ import org.rhasspy.mobile.viewmodel.element.MicrophoneFabViewModel
 class MicrophoneWidget : GlanceAppWidget(), KoinComponent {
 
     override val sizeMode: SizeMode = SizeMode.Exact
+
     val viewModel = get<MicrophoneFabViewModel>()
 
-    @Composable
-    @GlanceComposable
-    @Suppress("StateFlowValueCalledInComposition") //suppress because it doesn't work in glance
-    override fun Content() {
-        val viewState by viewModel.viewState.collectAsState()
+    override suspend fun provideGlance(context: Context, id: GlanceId) {
+        provideContent {
+            val viewState by viewModel.viewState.collectAsState()
 
-        //used to mimic border, drawable necessary for rounded corner on older devices
-        Box(
-            modifier = GlanceModifier
-                .appWidgetBackground()
-                .fillMaxSize()
-                .background(
-                    ImageProvider(
-                        if (viewState.isShowBorder) {
-                            R.drawable.microphone_widget_background_error
-                        } else getContainerForMicrophoneFabLegacy(
-                            isActionEnabled = viewState.isUserActionEnabled,
-                            isRecording = viewState.isRecording
-                        )
-                    )
-                ),
-            contentAlignment = Alignment.Center
-        ) {
-            val size = LocalSize.current
-
-            //inner content
+            //used to mimic border, drawable necessary for rounded corner on older devices
             Box(
                 modifier = GlanceModifier
-                    .size(size.width - 8.dp, size.height - 8.dp)
+                    .appWidgetBackground()
+                    .fillMaxSize()
                     .background(
                         ImageProvider(
-                            getContainerForMicrophoneFabLegacy(
+                            if (viewState.isShowBorder) {
+                                R.drawable.microphone_widget_background_error
+                            } else getContainerForMicrophoneFabLegacy(
                                 isActionEnabled = viewState.isUserActionEnabled,
                                 isRecording = viewState.isRecording
                             )
@@ -62,29 +46,46 @@ class MicrophoneWidget : GlanceAppWidget(), KoinComponent {
                     ),
                 contentAlignment = Alignment.Center
             ) {
-                //microphone icon
-                Image(
-                    modifier = GlanceModifier
-                        .size(48.dp),
-                    provider = ImageProvider(
-                        getMicrophoneFabIconLegacy(
-                            isMicOn = viewState.isShowMicOn,
-                            isActionEnabled = viewState.isUserActionEnabled,
-                            isRecording = viewState.isRecording
-                        )
-                    ),
-                    contentDescription = "translate(resource = MR.strings.microphone)",
-                    contentScale = ContentScale.FillBounds
-                )
-            }
+                val size = LocalSize.current
 
-            //box for clickable, else image is not clickable
-            Box(
-                modifier = GlanceModifier
-                    .fillMaxSize()
-                    .clickable(onClick = actionRunCallback<MicrophoneWidgetAction>()),
-                contentAlignment = Alignment.Center
-            ) {}
+                //inner content
+                Box(
+                    modifier = GlanceModifier
+                        .size(size.width - 8.dp, size.height - 8.dp)
+                        .background(
+                            ImageProvider(
+                                getContainerForMicrophoneFabLegacy(
+                                    isActionEnabled = viewState.isUserActionEnabled,
+                                    isRecording = viewState.isRecording
+                                )
+                            )
+                        ),
+                    contentAlignment = Alignment.Center
+                ) {
+                    //microphone icon
+                    Image(
+                        modifier = GlanceModifier
+                            .size(48.dp),
+                        provider = ImageProvider(
+                            getMicrophoneFabIconLegacy(
+                                isMicOn = viewState.isShowMicOn,
+                                isActionEnabled = viewState.isUserActionEnabled,
+                                isRecording = viewState.isRecording
+                            )
+                        ),
+                        contentDescription = "translate(resource = MR.strings.microphone)",
+                        contentScale = ContentScale.FillBounds
+                    )
+                }
+
+                //box for clickable, else image is not clickable
+                Box(
+                    modifier = GlanceModifier
+                        .fillMaxSize()
+                        .clickable(onClick = actionRunCallback<MicrophoneWidgetAction>()),
+                    contentAlignment = Alignment.Center
+                ) {}
+            }
         }
     }
 }
