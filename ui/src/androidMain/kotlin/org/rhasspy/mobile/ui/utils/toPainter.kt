b3079181679c8a2d1221fb1f8c06280c5e3edcfc
Nailik <13292441+Nailik@users.noreply.github.com>
2023-05-13 23:02:40 +0200

    removed now unused helper functions

--- ui/src/androidMain/kotlin/org/rhasspy/mobile/ui/utils/toPainter.kt
+++
@@ -1,8 +0,0 @@
-package org.rhasspy.mobile.ui.utils
-
-import androidx.compose.ui.graphics.painter.Painter
-import dev.icerock.moko.resources.ImageResource
-
-actual fun ImageResource.toPainter(isGreyscale: Boolean): Painter {
-    TODO("Not yet implemented")
-}
--- ui/src/commonMain/kotlin/org/rhasspy/mobile/ui/utils/toPainter.kt
+++
@@ -1,6 +0,0 @@
-package org.rhasspy.mobile.ui.utils
-
-import androidx.compose.ui.graphics.painter.Painter
-import dev.icerock.moko.resources.ImageResource
-
-expect fun ImageResource.toPainter(isGreyscale: Boolean = false) : Painter
