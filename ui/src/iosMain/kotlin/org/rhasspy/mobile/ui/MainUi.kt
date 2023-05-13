aaa0a71f2868bda3f59f17544adb0bed77d8e897
Nailik <13292441+Nailik@users.noreply.github.com>
2023-05-13 22:59:28 +0200

    fixed ios start page

--- ui/src/iosMain/kotlin/org/rhasspy/mobile/ui/MainUi.kt
+++ ui/src/iosMain/kotlin/org/rhasspy/mobile/ui/MainUi.kt
@@ -1,8 +1,9 @@
 package org.rhasspy.mobile.ui
 
-import androidx.compose.foundation.Image
+import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -10,46 +11,51 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.window.Application
-import dev.icerock.moko.resources.compose.painterResource
+import androidx.compose.ui.unit.dp
 import dev.icerock.moko.resources.desc.Resource
 import dev.icerock.moko.resources.desc.StringDesc
 import kotlinx.coroutines.flow.StateFlow
+import org.rhasspy.mobile.data.resource.stable
+import org.rhasspy.mobile.icons.RhasspyLogo
 import org.rhasspy.mobile.resources.MR
-import org.rhasspy.mobile.ui.content.elements.LocalImage
+import org.rhasspy.mobile.ui.content.elements.Icon
 
 @Composable
-fun MainUi(isHasStarted: StateFlow<Boolean>) { //TODO splash view
-    MaterialTheme {
-        if (isHasStarted.collectAsState().value) {
-            Box(modifier = Modifier.fillMaxSize()) {
-                FilledTonalButton(modifier = Modifier.align(Alignment.Center), onClick = {}) {
-                    Text(text = "Hello Compose Ui! - Material3")
-                    Text(text = StringDesc.Resource(MR.strings.backgroundServiceInformation).localized())
-                }
+fun MainUi(isHasStarted: StateFlow<Boolean>) {
 
-            }
-        } else {
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
-            ) {/*
-                Image(
-                    painter = painterResource(MR.images.ic_launcher),
-                    contentDescription = "text",
-                  //  modifier = modifier,
-                 //   alignment = alignment,
-                    contentScale = ContentScale.FillWidth,
-                   // alpha = alpha
-                )
-                LocalImage(
-                    imageResource = MR.images.ic_launcher,
-                    isGreyscale = true,
-                    contentDescription = "text",
-                    contentScale = ContentScale.FillWidth
-                )*/
+    MaterialTheme {
+
+        Crossfade(targetState = isHasStarted.collectAsState().value) { hasStarted ->
+            when (hasStarted) {
+                true -> StartScreen()
+                false -> SplashScreen()
             }
         }
+
+    }
+
+}
+
+@Composable
+private fun StartScreen() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        FilledTonalButton(modifier = Modifier.align(Alignment.Center), onClick = {}) {
+            Text(text = "Hello Compose Ui! - Material3")
+            Text(text = StringDesc.Resource(MR.strings.backgroundServiceInformation).localized())
+        }
+    }
+}
+
+@Composable
+private fun SplashScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Icon(
+            RhasspyLogo,
+            MR.strings.appName.stable,
+            Modifier.fillMaxSize().padding(24.dp)
+        )
     }
 }
