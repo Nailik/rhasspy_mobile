02d711f8aa121079691897dc6f663e0f60981823
Nailik <13292441+Nailik@users.noreply.github.com>
2023-05-19 18:02:49 +0200

    fixed hide microphone overlay eventually

--- androidApp/src/main/java/org/rhasspy/mobile/android/settings/content/MicrophoneOverlaySettings.kt
+++ androidApp/src/main/java/org/rhasspy/mobile/android/settings/content/MicrophoneOverlaySettings.kt
@@ -4,9 +4,13 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.expandVertically
 import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import org.rhasspy.mobile.android.content.elements.RadioButtonsEnumSelectionList
 import org.rhasspy.mobile.android.main.LocalViewModelFactory
 import org.rhasspy.mobile.android.permissions.RequiresOverlayPermission
@@ -41,57 +45,63 @@ fun MicrophoneOverlaySettingsContent() {
             title = MR.strings.microphoneOverlay.stable,
             onBackClick = { viewModel.onEvent(BackClick) }
         ) {
-
             Column {
 
                 val viewState by viewModel.viewState.collectAsState()
 
-                //overlay permission request
-                RequiresOverlayPermission(
-                    initialData = viewState.microphoneOverlaySizeOption,
-                    onClick = { viewModel.onEvent(SelectMicrophoneOverlaySizeOption(it)) }
-                ) { onClick ->
-
-                    //drop down to select option
-                    RadioButtonsEnumSelectionList(
-                        modifier = Modifier.testTag(TestTag.MicrophoneOverlaySizeOptions),
-                        selected = viewState.microphoneOverlaySizeOption,
-                        onSelect = { option ->
-                            if (option != MicrophoneOverlaySizeOption.Disabled) {
-                                //invoke permission request
-                                onClick.invoke(option)
-                            } else {
-                                //doesn't invoke permission request
-                                viewModel.onEvent(SelectMicrophoneOverlaySizeOption(option))
-                            }
-                        },
-                        values = viewState.microphoneOverlaySizeOptions
-                    )
-
-                }
-
-
-                val showOverlayWhileAppEnabled by remember { derivedStateOf { viewState.microphoneOverlaySizeOption != MicrophoneOverlaySizeOption.Disabled } }
-
-
-                //visibility of overlay while app
-                AnimatedVisibility(
-                    enter = expandVertically(),
-                    exit = shrinkVertically(),
-                    visible = showOverlayWhileAppEnabled
+                Card(
+                    modifier = Modifier.padding(8.dp),
+                    elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
                 ) {
+                    Column {
 
-                    SwitchListItem(
-                        modifier = Modifier.testTag(TestTag.VisibleWhileAppIsOpened),
-                        text = MR.strings.whileAppIsOpened.stable,
-                        isChecked = viewState.isMicrophoneOverlayWhileAppEnabled,
-                        onCheckedChange = { viewModel.onEvent(SetMicrophoneOverlayWhileAppEnabled(it)) }
-                    )
+                        //overlay permission request
+                        RequiresOverlayPermission(
+                            initialData = viewState.microphoneOverlaySizeOption,
+                            onClick = { viewModel.onEvent(SelectMicrophoneOverlaySizeOption(it)) }
+                        ) { onClick ->
+
+                            //drop down to select option
+                            RadioButtonsEnumSelectionList(
+                                modifier = Modifier.testTag(TestTag.MicrophoneOverlaySizeOptions),
+                                selected = viewState.microphoneOverlaySizeOption,
+                                onSelect = { option ->
+                                    if (option != MicrophoneOverlaySizeOption.Disabled) {
+                                        //invoke permission request
+                                        onClick.invoke(option)
+                                    } else {
+                                        //doesn't invoke permission request
+                                        viewModel.onEvent(SelectMicrophoneOverlaySizeOption(option))
+                                    }
+                                },
+                                values = viewState.microphoneOverlaySizeOptions
+                            )
+
+                        }
+
+                    }
+
+                    val showOverlayWhileAppEnabled by remember { derivedStateOf { viewState.microphoneOverlaySizeOption != MicrophoneOverlaySizeOption.Disabled } }
+
+                    //visibility of overlay while app
+                    AnimatedVisibility(
+                        enter = expandVertically(),
+                        exit = shrinkVertically(),
+                        visible = showOverlayWhileAppEnabled
+                    ) {
+
+                        SwitchListItem(
+                            modifier = Modifier.testTag(TestTag.VisibleWhileAppIsOpened),
+                            text = MR.strings.whileAppIsOpened.stable,
+                            isChecked = viewState.isMicrophoneOverlayWhileAppEnabled,
+                            onCheckedChange = { viewModel.onEvent(SetMicrophoneOverlayWhileAppEnabled(it)) }
+                        )
+
+                    }
 
                 }
 
             }
-
         }
     }
 
