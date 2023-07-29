a1916b4a3e68c7bc9978f73e7487ce8e15f60786
Nailik <13292441+Nailik@users.noreply.github.com>
2023-07-29 17:49:27 +0200

    more readable to string of audio source

--- platformspecific/src/commonMain/kotlin/org/rhasspy/mobile/platformspecific/audioplayer/AudioSource.kt
+++ platformspecific/src/commonMain/kotlin/org/rhasspy/mobile/platformspecific/audioplayer/AudioSource.kt
@@ -4,10 +4,18 @@ import dev.icerock.moko.resources.FileResource
 import okio.Path
 import org.koin.core.component.KoinComponent
 
-sealed interface AudioSource : KoinComponent {
+sealed class AudioSource : KoinComponent {
     @Deprecated("remove as soon as possible (when mqtt doesn't require it anymore)")
-    class Data(val data: ByteArray) : AudioSource
-    class File(val path: Path) : AudioSource
-    class Resource(val fileResource: FileResource) : AudioSource
+    class Data(val data: ByteArray) : AudioSource()
+    class File(val path: Path) : AudioSource()
+    class Resource(val fileResource: FileResource) : AudioSource()
+
+    override fun toString(): String {
+        return when (this) {
+            is Data     -> data.toString()
+            is File     -> path.toString()
+            is Resource -> fileResource.toString()
+        }
+    }
 
 }
