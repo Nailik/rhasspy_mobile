99da20da8f27a76fa661a0a38e9f102b1abbd9a3
Nailik <13292441+Nailik@users.noreply.github.com>
2023-08-05 16:04:32 +0200

    update versionnumber

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -2,7 +2,7 @@ object Version {
 
     const val major = 0
     const val minor = 5
-    const val patch = 0
+    const val patch = 1
     const val code = 107
 
     override fun toString(): String {
