eedc2b9b63b08ec2929ec30c5468b001e8f79bf8
Nailik <13292441+Nailik@users.noreply.github.com>
2023-08-05 21:39:42 +0200

    fix main screens loosing scroll state

--- ui/src/commonMain/kotlin/org/rhasspy/mobile/ui/main/SettingsScreen.kt
+++ ui/src/commonMain/kotlin/org/rhasspy/mobile/ui/main/SettingsScreen.kt
@@ -24,6 +24,8 @@ import org.rhasspy.mobile.ui.TestTag
 import org.rhasspy.mobile.ui.content.elements.*
 import org.rhasspy.mobile.ui.content.list.ListElement
 import org.rhasspy.mobile.ui.testTag
+import org.rhasspy.mobile.ui.utils.ListType.SettingsScreenList
+import org.rhasspy.mobile.ui.utils.rememberForeverLazyListState
 import org.rhasspy.mobile.viewmodel.navigation.NavigationDestination.MainScreenNavigationDestination.SettingsScreen
 import org.rhasspy.mobile.viewmodel.navigation.NavigationDestination.SettingsScreenDestination
 import org.rhasspy.mobile.viewmodel.navigation.NavigationDestination.SettingsScreenDestination.*
@@ -78,11 +80,14 @@ fun SettingsScreenContent(
         },
     ) { paddingValues ->
 
+        val lazyListState = rememberForeverLazyListState(SettingsScreenList)
+
         LazyColumn(
             Modifier
                 .testTag(TestTag.List)
                 .padding(paddingValues)
-                .fillMaxSize()
+                .fillMaxSize(),
+            state = lazyListState,
         ) {
 
             item {
