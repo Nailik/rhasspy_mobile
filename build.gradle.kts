26f0457c79d7bb16e2f7b0ae6e74087d2bc369ae
Nailik <13292441+Nailik@users.noreply.github.com>
2023-08-28 17:08:08 +0200

    dependency update to stable compose version

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,8 +27,8 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-        maven("https://androidx.dev/storage/compose-compiler/repository/")
+        //maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        //maven("https://androidx.dev/storage/compose-compiler/repository/")
         maven("https://jitpack.io")
     }
 }
