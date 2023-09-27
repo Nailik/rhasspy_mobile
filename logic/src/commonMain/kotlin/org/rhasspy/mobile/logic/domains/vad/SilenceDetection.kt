e47d7a5eb26f00201a6be737df98fca035c73c43
Nailik <13292441+Nailik@users.noreply.github.com>
2023-09-27 17:06:31 +0200

    finished local silence detection

--- logic/src/commonMain/kotlin/org/rhasspy/mobile/logic/domains/vad/SilenceDetection.kt
+++ logic/src/commonMain/kotlin/org/rhasspy/mobile/logic/domains/vad/SilenceDetection.kt
@@ -1,39 +1,39 @@
 package org.rhasspy.mobile.logic.domains.vad
 
-import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
 import org.rhasspy.mobile.logic.domains.mic.MicAudioChunk
-import org.rhasspy.mobile.logic.pipeline.PipelineEvent.AudioDomainEvent.AudioChunkEvent
+import kotlin.time.Duration
 
+/**
+ * selects silence by checking if audio volume is below a threshold (automaticSilenceDetectionAudioLevel)
+ * for a certain amount of time (automaticSilenceDetectionTime)
+ *
+ * this will only be checked after a specific time of recording (automaticSilenceDetectionMinimumTime) has passed
+ */
 class SilenceDetection(
-    private val automaticSilenceDetectionTime: Long?,
-    private val automaticSilenceDetectionMinimumTime: Long?,
+    private val automaticSilenceDetectionTime: Duration,
+    private val automaticSilenceDetectionMinimumTime: Duration,
     private val automaticSilenceDetectionAudioLevel: Float,
 ) {
 
     private var silenceStartTime: Instant? = null
-    private var recordingTillSilenceStartTime = Clock.System.now()
+    private var recordingStartTime: Instant? = null
 
     //returns true when silence was detected
     fun onAudioChunk(chunk: MicAudioChunk): Boolean {
-        return true /*
-        val automaticSilenceDetectionTime = automaticSilenceDetectionTime ?: 0
-        val automaticSilenceDetectionMinimumTime = automaticSilenceDetectionMinimumTime ?: 0
-
-        //check minimum recording time
-        val currentTime = Clock.System.now()
-        //if recordingTillSilenceStartTime is null there is an issue therefore timeSinceStart is set to 0
-        val timeSinceStart = currentTime.minus(recordingTillSilenceStartTime)
+        //set recording start time
+        val startTime = recordingStartTime ?: let {
+            recordingStartTime = chunk.timeStamp
+            chunk.timeStamp
+        }
 
         //minimum recording time not reached
-        if (timeSinceStart < automaticSilenceDetectionMinimumTime.milliseconds) return false
-/*
+        if (chunk.timeStamp.minus(startTime) < automaticSilenceDetectionMinimumTime) return false
+
         var max: Short = 0
         for (i in 0..chunk.data.size step 2) {
             if (i < chunk.data.size) {
-                val bb = ByteBuffer.wrap(chunk.data.copyOfRange(i, i + 2).nat)
-                bb.order(ByteOrder.nativeOrder())
-                val short = bb.short
+                val short = byteArrayToIntLittleEndian(chunk.data.copyOfRange(i, i + 2)).toShort()
 
                 if (short > max) {
                     max = short
@@ -42,27 +42,32 @@ class SilenceDetection(
         }
 
         //volume above threshold
-        if (volume > automaticSilenceDetectionAudioLevel) {
+        if (max > automaticSilenceDetectionAudioLevel) {
             //reset silence time (was above threshold)
             silenceStartTime = null
-            return
-        }*/
+            return false
+        }
 
         //volume below threshold
 
         //not initial silence
-        silenceStartTime?.also { silenceStart ->
-            //silence duration
-            val timeSinceSilenceDetected = currentTime.minus(silenceStart)
+        return silenceStartTime?.let { silenceStart ->
             //check if silence was detected for x milliseconds
-            if (timeSinceSilenceDetected >= automaticSilenceDetectionTime.milliseconds) {
-            //    onSilenceDetected()
-            }
+            chunk.timeStamp.minus(silenceStart) >= automaticSilenceDetectionTime
         } ?: run {
             //first time silence was detected
-            silenceStartTime = Clock.System.now()
+            silenceStartTime = chunk.timeStamp
+            false
         }
-*/
+
+    }
+
+    private fun byteArrayToIntLittleEndian(byteArray: ByteArray): Int {
+        var result = 0
+        for (i in byteArray.indices) {
+            result = result or (byteArray[i].toInt() and 0xFF shl (i * 8))
+        }
+        return result
     }
 
 }
