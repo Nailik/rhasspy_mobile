016ad049b3b13ca8f9e3bf3ab0f07d5b4caf9ba9
Nailik <13292441+Nailik@users.noreply.github.com>
2025-10-13 12:59:42 +0200

    update

--- gradle/conventions/src/main/kotlin/Version.kt
+++ gradle/conventions/src/main/kotlin/Version.kt
@@ -3,7 +3,7 @@ object Version {
     const val MAJOR = 0
     const val MINOR = 6
     const val PATCH = 2
-    const val CODE = 152
+    const val CODE = 153
 
     override fun toString(): String {
         return "$MAJOR.$MINOR.$PATCH-$CODE"
